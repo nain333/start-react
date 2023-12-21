60567c7a5080e959ef8ad55615553f88d912fd3b
nain333 <hnain70@gmail.com>
2023-12-21 16:09:01 +0530

    Empaty Fragments

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ const reactHeading = React.createElement("h1", { className: "head",id:"reactHead
 // Render the React element into the "root" element
 ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
 console.log('React Heading: ',reactHeading)
+/**
+ * JSX
+ * 
+ * */
+const jsxHeading=<>
+    <h1>
+        Hello  JSX
+    </h1>
+    <h3>
+        This is created using react!
+    </h3>
+
+</>
+ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+ 
